6b772de89af0225f7d9db66f8b5f394433ed3567
sclaw <klawson1@uvm.edu>
2024-04-22 11:10:05 -0400

    tidy metric logging

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -51,28 +51,60 @@ class Segment:
             return 0
         h = self.d / 2
         r = ((h ** 2) + (self.a ** 2)) / (2 * self.a)
-        alpha = np.arcsin(h / r)
+        if abs(h - r) < 0.0001:
+            alpha = np.pi / 2  # just kinda estimating by looking at the asymptote of arcsin on wolframalpha
+        else:
+            alpha = np.arcsin(h / r)
+        
         self.arc_l = r * alpha
         return (self.length - self.arc_l) / self.arc_l
     
 
 class Network:
 
-    def __init__(self, gdf, da, from_field='HYRIV_ID', to_field='NEXT_DOWN'):
+    def __init__(self, gdf, from_field='HYRIV_ID', to_field='NEXT_DOWN', order_field='UPLAND_SKM', root=None):
         self.gdf = gdf
-        self.da = da
+        self.gdf = self.gdf.set_index(from_field)
+        self.da = gdf[order_field].max()
         self.from_field = from_field
         self.to_field = to_field
-        self.edges = self.gdf[[from_field, to_field]]
 
-        self.gdf.loc[:, ['length', 'curvature', 'meander', 'orientation']] = np.nan
+        if root is None:
+            self.root = self.find_root()
+        else:
+            self.root = root
+
+        self.edge_dict = dict()
+        u = self.gdf.index.values
+        v = self.gdf[to_field].values
+        priority = {tmp_u: p for tmp_u, p in zip(u, self.gdf[order_field].values)}
+
+        edge_dict = dict()
+        for i in range(len(u)):
+            if v[i] in edge_dict:
+                if priority[u[i]] > priority[edge_dict[v[i]][0]]:
+                    edge_dict[v[i]].insert(0, u[i])
+                else:
+                    edge_dict[v[i]].append(u[i])
+            else:
+                edge_dict[v[i]] = [u[i]]
+        self.edge_dict = edge_dict
+
+        self.gdf.loc[:, ['length', 'curvature', 'meander', 'orientation', 'depth', 'leaves', 'balance_factor', 'cum_depth', 'ave_depth', 'tja']] = np.nan
         self.calc_edge_metrics()
 
+        self.post_order_traversal()
         self.metrics = self.calc_network_metrics()
 
+    def find_root(self):
+        root = self.gdf[self.gdf[self.to_field].isin(self.gdf.index) == False][self.to_field].unique()
+        if len(root) > 1:
+            raise ValueError('Multiple roots found')
+        return root[0]
+
     def calc_edge_metrics(self):
-        for i in range(len(self.gdf)):
-            i = self.gdf.index[i]
+        print('Calculating Edge Metrics...')
+        for i in self.gdf.index:
             tmp_e = Segment(self.gdf.loc[i, 'geometry'])
             self.gdf.loc[i, 'length'] = tmp_e.length
             self.gdf.loc[i, 'curvature'] = tmp_e.curvature()
@@ -80,6 +112,7 @@ class Network:
             self.gdf.loc[i, 'orientation'] = tmp_e.orientation
 
     def calc_network_metrics(self):
+        print('Calculating Network Metrics...')
         out_dict = dict()
 
         out_dict['ave_length'] = self.gdf['length'].mean()
@@ -95,11 +128,21 @@ class Network:
         out_dict['med_orientation'] = self.gdf['orientation'].median()
         out_dict['std_orientation'] = self.gdf['orientation'].std()
 
-        tjas = self.calc_junction_angles()
-        tjas = np.array([i for i in tjas.values()])
+        tjas = self.gdf['tja'].dropna()
         out_dict['ave_tja'] = tjas.mean()
-        out_dict['med_tja'] = np.median(tjas)
+        out_dict['med_tja'] = tjas.median()
         out_dict['std_tja'] = tjas.std()
+
+        out_dict['med_depth'] = self.gdf['depth'].median()
+        out_dict['std_depth'] = self.gdf['depth'].std()
+        out_dict['ave_balance'] = self.gdf['balance_factor'].mean()
+        out_dict['med_balance'] = self.gdf['balance_factor'].median()
+        out_dict['std_balance'] = self.gdf['balance_factor'].std()
+
+        out_dict['leaves'] = self.gdf.loc[self.root, 'leaves']
+        out_dict['ave_depth'] = self.gdf.loc[self.root, 'ave_depth']
+        out_dict['height'] = self.gdf['depth'].max()
+        out_dict['compactness'] = out_dict['height'] / out_dict['leaves']
 
         out_dict['density'] = self.gdf['length'].sum() / self.da
         out_dict['texture'] = len(self.gdf) / self.da
@@ -111,17 +154,18 @@ class Network:
         return out_dict
 
     def bifurcation_ratios(self):
-        present_orders = sorted(self.gdf['ORD_STRA'].unique())
+        present_orders = sorted(self.gdf['ORD_STRA'].dropna().unique())
         out_dict = dict()
         for i in range(len(present_orders) - 1):
             i += 2
             out_dict[f'bifurcation_{i}'] = np.sum(self.gdf['ORD_STRA'] == i - 1) / np.sum(self.gdf['ORD_STRA'] == i)
-        out_dict['mean'] = np.sum(self.gdf['ORD_STRA'] == 1) ** (1 / (max(present_orders) - 1))
+        out_dict['bifurcation_mean'] = np.sum(self.gdf['ORD_STRA'] == 1) ** (1 / (max(present_orders) - 1))
         return out_dict
 
     def calc_junction_angles(self):
-        init_list = self.edges.merge(self.edges, left_on=self.from_field, right_on=self.to_field, how='left')
-        init_list = init_list[init_list[self.from_field + '_y'].isna()][self.to_field + '_x'].unique()
+        init_list = self.gdf[[self.to_field]]
+        init_list = init_list.merge(init_list, left_index=True, right_on=self.to_field, how='left')
+        init_list = init_list[init_list[self.to_field + '_y'].isna()][self.to_field + '_x'].unique()
 
         q = queue.Queue()
         [q.put(i) for i in init_list]
@@ -144,6 +188,49 @@ class Network:
             if not next_down in evaluated:
                 q.put(next_down)
         return evaluated
+    
+    def _process_node(self, node):
+        if node not in self.edge_dict:
+            self.gdf.loc[node, 'depth'] = 0
+            self.gdf.loc[node, 'leaves'] = 1
+            self.gdf.loc[node, 'balance_factor'] = 0
+            self.gdf.loc[node, 'cum_depth'] = 0
+            self.gdf.loc[node, 'ave_depth'] = 0
+            self.gdf.loc[node, 'tja'] = np.nan
+        else:
+            children = self.edge_dict[node]
+            sub_df = self.gdf.loc[children]
+            depths = sub_df['depth'].values
+            self.gdf.loc[node, 'depth'] = depths.max() + 1
+            leaves = sub_df['leaves'].sum()
+            self.gdf.loc[node, 'leaves'] = leaves
+            cum_depth = sub_df['cum_depth'].sum() + leaves
+            self.gdf.loc[node, 'cum_depth'] = cum_depth
+            self.gdf.loc[node, 'ave_depth'] = cum_depth / leaves
+            self.gdf.loc[node, 'balance_factor'] = depths[0] - depths[1]
+            orientations = sub_df['orientation'].values
+            tja = abs(orientations[0] - orientations[1])
+            if tja > 180:
+                tja = 360 - tja
+            self.gdf.loc[node, 'tja'] = tja
+            
+    def post_order_traversal(self):
+        print('Traversing Network...')
+        
+        q = [self.root]
+        visited = []
+        while q:
+            cur_node = q[-1]
+            if cur_node not in self.edge_dict or all([child in visited for child in self.edge_dict[cur_node]]):
+                q.pop()
+                self._process_node(cur_node)
+                visited.append(cur_node)
+            else:
+                for child in self.edge_dict[cur_node]:
+                    if child not in visited:
+                        q.append(child)
+        return visited
+
 
 
 
